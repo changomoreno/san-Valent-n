9faeea061da9decf95d9e9b3c2c5f06ef6fb1de0
changomoreno <changomoreno903@gmail.com>
2026-02-14 01:33:00 -0400

    Add files via upload

---
+++ flores-main/script.js
@@ -0,0 +1,73 @@
+const container = document.getElementById('flowerContainer');
+
+const messageElement = document.querySelector('.message-text');
+
+const poemLines = [
+    "Hoy dia del amor qusiera estar contigo...",
+    "Aunque hoy el calendario me recuerde que no estás,",
+    "mi corazón te siente cerca, mucho más que ayer...",
+    "La distancia es solo un número, un simple esperar,",
+    "porque en cada pensamiento, te vuelvo a tener...",
+    "No importa el mapa, ni el reloj, ni este inmenso mar,",
+    "cada día te amo más, con paciencia y esperanza.",
+    "Feliz San Valentín, mi amor, allá donde estés,",
+    "que mi alma te acompaña y sueña con tu abrazo.",
+];
+
+let currentLineIndex = 0;
+
+const displayTime = 3000;
+const fadeTime = 1500;
+
+function showNextLine() {
+    if (currentLineIndex < poemLines.length - 1) {
+        messageElement.classList.remove('fade-in');
+        messageElement.classList.add('fade-out');
+
+        setTimeout(() => {
+            currentLineIndex++;
+
+            messageElement.textContent = poemLines[currentLineIndex];
+
+            messageElement.classList.remove('fade-out');
+            messageElement.classList.add('fade-in');
+        }, fadeTime);
+    } else {
+        // Iniciar las flores después de mostrar el mensaje completo
+        setTimeout(() => {
+            setInterval(createFlower, 250);
+        }, fadeTime);
+    }
+}
+
+setTimeout(() => {
+    messageElement.textContent = poemLines[currentLineIndex];
+    messageElement.classList.add('fade-in');
+}, 1000);
+
+setInterval(showNextLine, displayTime + fadeTime);
+
+function createFlower() {
+    const flowerWrapper = document.createElement('div');
+    flowerWrapper.classList.add('flower-wrapper');
+
+    const flower = document.createElement('div');
+    flower.classList.add('flower');
+
+    flowerWrapper.appendChild(flower);
+    flowerWrapper.style.left = Math.random() * 95 + 'vw';
+    flowerWrapper.style.animationDuration = (Math.random() * 8 + 7) + 's';
+    flower.style.animationDuration = (Math.random() * 2 + 3) + 's';
+
+    const size = Math.random() * 30 + 10;
+    flower.style.width = size + 'px';
+    flower.style.height = size + 'px';
+    flower.style.filter = `blur(${Math.random() * 2}px)`;
+    flower.style.opacity = Math.random() * 0.5 + 0.5;
+
+    container.appendChild(flowerWrapper);
+
+    setTimeout(() => {
+        flowerWrapper.remove();
+    }, parseFloat(flowerWrapper.style.animationDuration) * 1000 + 500);
+}
